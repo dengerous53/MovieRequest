46d3a49a7693c83276534905bab41a57462c065b
Dengerous <99312935+dengerous53@users.noreply.github.com>
2022-09-12 14:58:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Enox5600/MovieRequest.git /EvaMaria
+  git clone https://github.com/dengerous53/MovieRequest.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
